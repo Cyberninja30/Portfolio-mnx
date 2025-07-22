daa023067a4d54675ecf7f9f883a934c0d763ecf
Moinak_Mnx <111623431+Cyberninja30@users.noreply.github.com>
2025-07-22 10:40:48 +0530

    Update project.jsx

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,14 +1,22 @@
 import { useNavigate } from "react-router-dom";
 
 const Projects = () => {
-  const projects = [
-    ['2025', 'NetProbe – Networking Simulation Toolkit', 'A hands-on networking simulator to enhance networking skills via problem-solving and lab automation. Tech stack: Cisco Packet Tracer, advanced web tools (HTML/CSS/JS), and automation logic.'],
-    ['2025', 'Miss Robot – 403 Forbidden Bypass Tool', 'A penetration testing utility to bypass restricted directories using logic-based automation. Tech stack: Python, Bash, HTTP header manipulation, and directory fuzzing.'],
-    ['2025', 'Network Analyzer – Full-stack Recon & Logger Tool', 'Combines packet sniffing, keylogging, and web scraping to analyze traffic and log user input for research and audit purposes. Built using Python, BeautifulSoup, and socket programming.'],
-    ['2024', 'ThreatHunter Pro – Advanced SIEM Analytics', 'Open‑source threat hunting with ML‑powered anomaly detection (Python, ELK, React); used by 500+ teams.'],
-    ['2023', 'Zero‑Day Discovery in Smart Home Devices', 'Found critical IoT vulnerabilities; CVE‑2023‑XXXX; published at Black Hat USA 2023.'],
-    ['2022', 'Enterprise SOC Transformation', 'Modernized SOC for 10k+ organization; reduced MTTR by 75% and false‑positives by 60%.']
-  ];
+ const projects = {
+    '2022': [
+      ['NetProbe – Networking Simulation Toolkit', 'A hands-on networking simulator to enhance networking skills via problem-solving and lab automation. Tech stack: Cisco Packet Tracer, advanced web tools (HTML/CSS/JS), and automation logic.'],
+      ['Miss Robot – 403 Forbidden Bypass Tool', 'A penetration testing utility to bypass restricted directories using logic-based automation. Tech stack: Python, Bash, HTTP header manipulation, and directory fuzzing.'],
+      ['Network Analyzer – Full-stack Recon & Logger Tool', 'Combines packet sniffing, keylogging, and web scraping to analyze traffic and log user input for research and audit purposes. Built using Python, BeautifulSoup, and socket programming.']
+    ],
+    '2023': [
+      ['ThreatHunter Pro – Advanced SIEM Analytics', 'Open‑source threat hunting with ML‑powered anomaly detection (Python, ELK, React); used by 500+ teams.']
+    ],
+    '2024': [
+      ['Zero‑Day Discovery in Smart Home Devices', 'Found critical IoT vulnerabilities; CVE‑2023‑XXXX; published at Black Hat USA 2023.']
+    ],
+    '2025': [
+      ['Enterprise SOC Transformation', 'Modernized SOC for 10k+ organization; reduced MTTR by 75% and false‑positives by 60%.']
+    ]
+  };
 
     return (
 
